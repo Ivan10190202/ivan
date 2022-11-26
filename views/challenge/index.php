512716f10c9538b207857eb04932cb85b009c828
Иван Третьяк <ivan.varvara.03@mail.ru>
2022-11-26 21:39:22 +0300

    26.11

--- views/challenge/index.php
+++ views/challenge/index.php
@@ -10,6 +10,8 @@ use yii\widgets\Pjax;
 /** @var app\models\ChallengeSearch $searchModel */
 /** @var yii\data\ActiveDataProvider $dataProvider */
 
+$this->title = 'Личный кабинет';
+
 $this->title = 'Список брошенных вызовов';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
@@ -21,32 +23,17 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= Html::a('Бросить вызов', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
-    <?php Pjax::begin(); ?>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            
-            'title',
-            'description:ntext',
-            'id_tema',
-            //'id_chat',
-            'id_vicrory',
-            //'data_create',
-            'data_close',
-            [
-                'class' => ActionColumn::className(),
-                'urlCreator' => function ($action, Challenge $model, $key, $index, $column) {
-                    return Url::toRoute([$action, 'id_challenge' => $model->id_challenge]);
-                 }
-            ],
-        ],
-    ]); ?>
-
-    <?php Pjax::end(); ?>
+<div class="user-index">
+    <h1 style="margin-top: 100px;" ><?= Html::encode($this->title) ?></h1>
+    <div style="padding: 30px; width: 100%; background-color: #343A40; border-radius: 30px; margin-top:60px;">
+        <p style=" color: white; font-size: 20px; margin-bottom: 0px;">Ваш ник: </p>
+        <h3 style="color: white; font-size: 28px;"><?=Yii::$app->user->identity->username;?></h3>
+        <p style="color: white; font-size: 20px; margin-top: 30px; margin-bottom: 0px;">Почта: </p>
+        <h3 style="color: white; font-size: 28px;  margin-bottom: 30px;"><?=Yii::$app->user->identity->email;?></h3>
+        <p style="color: white; font-size: 20px; margin-top: 30px; margin-bottom: 0px;">Ваш зашифрованный пароль: </p>
+        <h3 style="color: white; font-size: 28px; width:100%;"><?=Yii::$app->user->identity->password_hash;?></h3>    
+    </div>
+</div>
 
 </div>
